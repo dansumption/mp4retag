a012495e46a6ce1a83efd98aa901d326dd324140
Dan Sumption <dan@sumption.org>
2017-07-29 20:51:03 +0100

    Reads tags from a directory

--- app.js
+++ app.js
@@ -1,60 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var fs = require('fs');
+var mm = require('musicmetadata');
+ 
+var path = "N:/Poetry/T_S_Eliot_Reads_Old_Possum_s_Book_of_Practical_Cats/";
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+var handleItem = (filename) => {
+  var item = path + filename;
+  console.log("Reading ", item);
+  var stream = fs.createReadStream(item);
+  var parser = mm(stream, function (err, metadata) {
+  console.log("Data for " + item)
+  if (err) {
+    console.log (err);
+  }
+  console.log(metadata);
+  stream.close();
 });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+fs.readdir(path, function(err, items) {
+    // console.log(items);
+ 
+    for (var i=0; i<items.length; i++) {
+        handleItem(items[i]);
+    }
 });
-
-
-module.exports = app;
