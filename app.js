cf1d8cd813bf4f3e77a73daa5efe47c3ef3a4331
Dan Sumption <dan@sumption.org>
2017-07-29 21:48:37 +0100

    Reads genre

--- app.js
+++ app.js
@@ -2,24 +2,42 @@ var fs = require('fs');
 var jsmediatags = require("jsmediatags");
  
 // var path = "N:/Poetry/T_S_Eliot_Reads_Old_Possum_s_Book_of_Practical_Cats/";
-var path = "N:/zzzTag/";
+var path = "N:/zzzTag/temp/";
 
-var handleItem = (filename) => {
-  var item = path + filename;
+var readFile = (filename) => {
+  var filePath = path + filename;
   // console.log("Reading ", item);
-  jsmediatags.read(item, {
+  jsmediatags.read(filePath, {
     onSuccess: function(tag) {
-      console.log(item, tag);
+      // console.log(filePath, tag);
+      processFile({file: filePath, tags: tag['tags']})
+      // console.log(JSON.stringify(tag['tags']));
     },
     onError: function(error) {
-      console.log(':(', error.type, error.info);
+      console.log(':(',filePath , error.type, error.info);
     }
   });
 };
+
+var logObject = (obj) => {
+  for (var k in obj) {
+    console.log(k);
+  } 
+}
+
+var processFile = ({file, tags}) => {
+  var genre = findGenre(tags);
+  console.log("Processed ", file, genre);
+};
+
+var findGenre = (tags) => {
+  return tags['genre'];
+}
+
 fs.readdir(path, function(err, items) {
     // console.log(items);
  
     for (var i=0; i<items.length; i++) {
-        handleItem(items[i]);
+        readFile(items[i]);
     }
 });
