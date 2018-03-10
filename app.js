6802f9ca3dcb87b77236a83be333862b5eef35a3
Dan Sumption <dan@sumption.org>
2018-03-10 11:12:29 +0000

    Record time taken and numer of files processed

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const _ = require("lodash/fp");
 const debug = require('./debug');
 const m4aFile = require('./m4aFile');
 
-const readDir = "n:/zzzTag/temp";
-// const readDir = "C:/Users/dan/Desktop/iPlayer Recordings";
-const writeDir = "C:/Users/dan/Desktop/iPlayer tagged";
-const completeDir  = [readDir, "complete"].join('/');
+const startTime = new Date();
+const readDir = "n:/zzzTag/allfiles";
+const writeDir = "n:/zzzTag/output";
+// const readDir = "C:/Users/dan/Desktop/iPlayer Recordings/temp";
+// const writeDir = "C:/Users/dan/Desktop/iPlayer tagged";
+const completeDir = [readDir, "complete"].join('/');
 const failDir = [readDir, "failed"].join('/');
 const filesToMatch = /\.m4a$/;
+
 
 m4aFile.setDefaults({
     readDir,
@@ -18,15 +21,33 @@ m4aFile.setDefaults({
 })
 
 let chain = Promise.resolve();
+let filesProcessed = 0;
+let filesSkipped = 0;
 
-fs.readdir(readDir, function(err, files) {
-    _.forEach(function(filename) {
+fs.readdir(readDir, function (err, files) {
+    _.forEach(function (filename) {
         if (filesToMatch.exec(filename)) {
-            chain = chain.then(function() {
+            chain = chain.then(function () {
+                filesProcessed++;
                 return m4aFile.processFile(filename)
             });
         } else {
+            filesSkipped++;
             debug("Skipping: " + filename);
         }
     }, files);
+
+    chain = chain.then(function () {
+        const endTime = new Date();
+        let timeDiff = (endTime - startTime) / 1000;
+        const seconds = Math.round(timeDiff % 60);
+        timeDiff = Math.floor(timeDiff / 60);
+        const minutes = Math.round(timeDiff % 60);
+        timeDiff = Math.floor(timeDiff / 60);
+        const hours = Math.round(timeDiff % 24);
+        timeDiff = Math.floor(timeDiff / 24);
+        const days = timeDiff;
+    
+        debug(`Processed ${filesProcessed} files, and skipped ${filesSkipped}.`, `In ${days} days, ${hours}:${minutes}:${seconds}`);
+    })
 });
